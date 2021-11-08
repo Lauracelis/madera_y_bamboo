3e870f8320a95c5abffc6a246694dd190ced7497
Laura Celis <lauracelis436@gmail.com>
2021-11-07 20:46:40 -0500

    Instale paquetes y cree form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,34 @@
 import React from 'react';
+/*import Header from '.../Header.js';*/
 
-const Contact = () => {
-    <div>
-    <h1>Contacto</h1>
-    </div>
+export default function Contact(); {
+    Return(
+        <>
+            <Head>
+                <title>Comunicate </title>
+                <ink rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1>Contact Us</h1>
+            <p>lorem ipsum</p>
+
+            <div id="contact-container">
+                <div classname="contact-info">
+                    <h4>contact information</h4>
+                </div>
+                <p>text</p>
+                <div classname="icon-text">
+                    <i classname="icon" />
+                    <span>text</span>
+                </div>
+                <div classname="social-media">
+                    <a href="#" classname="icon-circle" />
+                    <i classname="icon" />
+                </div>
+                <form>
+                </form>
+
+            </div>
+        </>
+    )
 }
-
-export default Contact;
