64bc58eeef28f5de96f64ae5838ad45f4658d7ab
laura <lauracelis436@gmail.com>
2021-11-23 21:47:29 -0500

    Mejore las secciones y componentes

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,32 +3,60 @@ import React from 'react';
 
 export default function Contact() {
     return(
-        <div>
-            <div>
-                <title>Comunicate </title>
-                <ink rel="icon" href="/favicon.ico" />
-            </div>
-
-            <h1>Contact Us</h1>
-            <p>lorem ipsum</p>
-
-            <div id="contact-container">
-                <div classname="contact-info">
-                    <h4>contact information</h4>
-                </div>
-                <p>text</p>
-                <div classname="icon-text">
-                    <i classname="icon" />
-                    <span>text</span>
-                </div>
-                <div classname="social-media">
-                    <a href="#" classname="icon-circle" />
-                    <i classname="icon" />
-                </div>
-                <form>
-                </form>
-
-            </div>
+        <>
+        <div className="contacto">
+        <div className="contactinfo">
+          <h2>Información de contacto</h2>
+          <ul className="info">
+            <li>
+              <span>{/*<img src={} className="icono" alt="Ubicación">/*/}</span>
+              <span>Venecia
+                Bogotá,Colombia
+                calle 42b #72h sur 15</span>
+            </li>
+            <li>
+              <span>{/*<img src={} className="icono" alt="Correo">/*/}</span>
+              <span>maderaybamboo@gmail.com</span>
+            </li>
+            <li>
+              <span>{/*<img src={} className="icono" alt="Celular">/*/}</span>
+              <span>3058696361</span>
+            </li>
+          </ul>
+          <ul className="sci">
+            <li>{/*<a href="#"></a><img src="instagram"/>*/}</li>
+            <li>{/*<a href="#"></a><img src="facebook"/>*/}</li>
+          </ul>
         </div>
+      </div>
+      <div className="contactForm">
+        <h2>Envianos un mensaje</h2>
+        <div className="formBox">
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>Nombres</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>Apellidos</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>correo</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>celular</span>
+          </div>
+          <div className="inputBoxx">
+            <textarea name="" required></textarea>
+            <span>Escribe aquí tu mensaje</span>
+          </div>
+          <div className="inputBoxx">
+            <input type="submit" value="send" />
+          </div>
+        </div>
+      </div>
+    </>
     )
 }
--- src/Pages/PrincipalPage/PPage.js
+++ src/Pages/PrincipalPage/PPage.js
@@ -19,6 +19,9 @@ const Ppage = () => (
       <div className="punto1">
         <p>punto 3</p>
       </div>
+      <div className="punto1">
+        <p>punto 3</p>
+      </div>
     </section>
 
     {/*Buscar hacer un grid aquí*/}
@@ -52,76 +55,13 @@ const Ppage = () => (
       </div>
     </section>
 
-    <section className="puntos_a_favor_2.0">
+    <section className="llamadaALaAcción">
       <div className="punto1">
         <p>punto 1</p>
       </div>
-      <div className="punto1">
-        <p>punto 2</p>
-      </div>
-      <div className="punto1">
-        <p>punto 3</p>
-      </div>
     </section>
 
-    <section>
-      <div className="contacto">
-        <div className="contactinfo">
-          <h2>Información de contacto</h2>
-          <ul className="info">
-            <li>
-              <span>{/*<img src={} className="icono" alt="Ubicación">/*/}</span>
-              <span>Venecia
-                Bogotá,Colombia
-                calle 42b #72h sur 15</span>
-            </li>
-            <li>
-              <span>{/*<img src={} className="icono" alt="Correo">/*/}</span>
-              <span>maderaybamboo@gmail.com</span>
-            </li>
-            <li>
-              <span>{/*<img src={} className="icono" alt="Celular">/*/}</span>
-              <span>3058696361</span>
-            </li>
-          </ul>
-          <ul className="sci">
-            <li>{/*<a href="#"></a><img src="instagram"/>*/}</li>
-            <li>{/*<a href="#"></a><img src="facebook"/>*/}</li>
-          </ul>
-        </div>
-      </div>
-      <div className="contactForm">
-        <h2>Envianos un mensaje</h2>
-        <div className="formBox">
-          <div className="inputBox">
-            <input type="text" name="" required />
-            <span>Nombres</span>
-          </div>
-          <div className="inputBox">
-            <input type="text" name="" required />
-            <span>Apellidos</span>
-          </div>
-          <div className="inputBox">
-            <input type="text" name="" required />
-            <span>correo</span>
-          </div>
-          <div className="inputBox">
-            <input type="text" name="" required />
-            <span>celular</span>
-          </div>
-          <div className="inputBoxx">
-            <textarea name="" required></textarea>
-            <span>Escribe aquí tu mensaje</span>
-          </div>
-          <div className="inputBoxx">
-            <input type="submit" value="send" />
-          </div>
-        </div>
-      </div>
-
-    </section>
-
-
+  
   </>
 )
   
