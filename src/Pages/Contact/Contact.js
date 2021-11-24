cb9d8cffcd1f8ff92f5e0d50907608eb1812cf37
laura <lauracelis436@gmail.com>
2021-11-23 22:45:11 -0500

    enrute el about us

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+
+const Contact = () => {
+    return(
+        <>
+        <div className="contacto">
+        <div className="contactinfo">
+          <h2>Información de contacto</h2>
+          <ul className="info">
+            <li>
+              <span>{/*<img src={} className="icono" alt="Ubicación">/*/}</span>
+              <span>Venecia
+                Bogotá,Colombia
+                calle 42b #72h sur 15</span>
+            </li>
+            <li>
+              <span>{/*<img src={} className="icono" alt="Correo">/*/}</span>
+              <span>maderaybamboo@gmail.com</span>
+            </li>
+            <li>
+              <span>{/*<img src={} className="icono" alt="Celular">/*/}</span>
+              <span>3058696361</span>
+            </li>
+          </ul>
+          <ul className="sci">
+            <li>{/*<a href="#"></a><img src="instagram"/>*/}</li>
+            <li>{/*<a href="#"></a><img src="facebook"/>*/}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="contactForm">
+        <h2>Envianos un mensaje</h2>
+        <div className="formBox">
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>Nombres</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>Apellidos</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>correo</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="" required />
+            <span>celular</span>
+          </div>
+          <div className="inputBoxx">
+            <textarea name="" required></textarea>
+            <span>Escribe aquí tu mensaje</span>
+          </div>
+          <div className="inputBoxx">
+            <input type="submit" value="send" />
+          </div>
+        </div>
+      </div>
+    </>
+    )
+}
+export default Contact;
